dfef121da96623177be26ded0efe8d51b00c2349
Alireza <alirezakhalili1380.12.11@gmail.com>
2025-11-10 14:37:04 +0330

    Impelimention Generic Design Pattern

---
+++ RepositoryDesignPattern/Frameworks/Bases/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using RepositoryDesignPattern.Frameworks.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryDesignPattern.Frameworks.Bases;
+
+internal class GenericRepository<TDbContext, TEntity, TPrimaryKey> : IGenericRepository<TEntity, TPrimaryKey>
+                                                                          where TEntity : class
+                                                                          where TDbContext : IdentityDbContext<User, Role, string,
+                                                                              IdentityUserClaim<string>, UserRole, IdentityUserLogin<string>,
+                                                                              IdentityRoleClaim<string>, IdentityUserToken<string>>
+{
+    #region Filds
+    protected virtual TDbContext DbContext { get; set; }
+    #endregion
+
+    #region Constructor
+
+    public GenericRepository()
+    {
+        
+    }
+    #endregion
+}
