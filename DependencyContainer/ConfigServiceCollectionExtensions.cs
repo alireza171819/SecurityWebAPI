d93621afaef0406a46fd4a564d920316e2c5de22
Alireza <alirezakhalili1380.12.11@gmail.com>
2025-11-10 14:34:56 +0330

    Create DependencyContainer.csproj Create ConfigServiceCollectionExtensions.cs

---
+++ DependencyContainer/ConfigServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigServiceCollectionExtensions
+{
+    public static IServiceCollection AddConfig(
+             this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<PositionOptions>(
+            config.GetSection(PositionOptions.Position));
+
+        return services;
+    }
+
+    public static IServiceCollection AddDependencyGroup(
+         this IServiceCollection services)
+    {
+        services.AddScoped<IMyDependency, MyDependency>();
+        services.AddScoped<IMyDependency2, MyDependency2>();
+
+        return services;
+    }
+}
