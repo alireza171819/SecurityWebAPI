8084e583812d8acbedc675b4daf75c94b4134ad1
Alireza <alirezakhalili1380.12.11@gmail.com>
2025-11-18 16:40:40 +0330

    Update ConfigServices

--- DependencyContainer/ConfigServiceCollectionExtensions.cs
+++ DependencyContainer/ConfigServiceCollectionExtensions.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using ApplicationService.Services.ProductServices;
+using ApplicationService.Services.ProductServices.Contract;
+using Domain.Aggregates.UserManagementAggregates;
+using EfCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using RepositoryDesignPattern.Contracts;
+using RepositoryDesignPattern.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -7,17 +15,30 @@ public static class ConfigServiceCollectionExtensions
     public static IServiceCollection AddConfig(
              this IServiceCollection services, IConfiguration config)
     {
-        services.Configure<PositionOptions>(
-            config.GetSection(PositionOptions.Position));
+       
+        services.AddDbContext<SecurityWebAPIContext>(options =>
+        {
+            options.UseSqlServer(config.GetConnectionString("WebAPIApplicationConnectionString"));
+        });
 
+
+        services.AddIdentity<User, Role>().AddEntityFrameworkStores<SecurityWebAPIContext>();
+        services.Configure<IdentityOptions>(c =>
+        {
+            c.Password.RequireDigit = false;
+            c.Password.RequireNonAlphanumeric = false;
+            c.Password.RequiredLength = 3;
+            c.Password.RequireUppercase = false;
+            c.Password.RequireLowercase = false;
+        });
         return services;
     }
 
     public static IServiceCollection AddDependencyGroup(
          this IServiceCollection services)
     {
-        services.AddScoped<IMyDependency, MyDependency>();
-        services.AddScoped<IMyDependency2, MyDependency2>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IProductService, ProductService>();
 
         return services;
     }
